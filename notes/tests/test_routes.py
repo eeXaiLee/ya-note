b77a2fd3028ff3a864e0f564e6e1791a7dab8de3
Evgeniy Dimitriev <dimitriev_ev@mail.ru>
2025-06-28 18:28:17 +0500

    Implement routes tests

---
+++ notes/tests/test_routes.py
@@ -0,0 +1,87 @@
+from http import HTTPStatus
+
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+from notes.models import Note
+
+
+User = get_user_model()
+
+
+class TestRoutes(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.author = User.objects.create(username='Автор')
+        cls.user_1 = User.objects.create(username='Пользователь_1')
+        cls.note = Note.objects.create(
+            title='Заголовок',
+            text='Текст',
+            author=cls.author
+        )
+
+    def test_pages_availability(self):
+        """Публичные страницы доступны любому пользователю."""
+        urls = (
+            ('notes:home', 'get'),
+            ('users:login', 'get'),
+            ('users:logout', 'post'),
+            ('users:signup', 'get'),
+        )
+        for name, method in urls:
+            with self.subTest(name=name):
+                url = reverse(name)
+                client_method = getattr(self.client, method)
+                response = client_method(url)
+                self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_redirect_for_anonymous_user(self):
+        """Анонимный пользователь перенаправляется на страницу входа."""
+        login_url = reverse('users:login')
+        urls = (
+            ('notes:list', None),
+            ('notes:add', None),
+            ('notes:detail', (self.note.slug,)),
+            ('notes:edit', (self.note.slug,)),
+            ('notes:delete', (self.note.slug,)),
+        )
+        for name, args in urls:
+            with self.subTest(name=name):
+                url = reverse(name, args=args)
+                redirect_url = f'{login_url}?next={url}'
+                response = self.client.get(url)
+                self.assertRedirects(response, redirect_url)
+
+    def test_availability_for_authorized_users(self):
+        """Страницы list, add, detail доступны авторизованному пользователю."""
+        self.client.force_login(self.author)
+        urls = (
+            ('notes:list', None),
+            ('notes:add', None),
+            ('notes:detail', (self.note.slug,)),
+        )
+        for name, args in urls:
+            with self.subTest(name=name):
+                url = reverse(name, args=args)
+                response = self.client.get(url)
+                self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_availability_for_notes_edit_and_delete(self):
+        """Проверка доступа к edit/delete для разных пользователей."""
+        users_statuses = (
+            (self.author, HTTPStatus.OK),
+            (self.user_1, HTTPStatus.NOT_FOUND),
+        )
+        urls = (
+            'notes:edit',
+            'notes:delete',
+        )
+        for user, status in users_statuses:
+            self.client.force_login(user)
+            for name in urls:
+                with self.subTest(user=user, name=name):
+                    url = reverse(name, args=(self.note.slug,))
+                    response = self.client.get(url)
+                    self.assertEqual(response.status_code, status)
