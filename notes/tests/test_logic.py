d37a09f3dac429060f07faf76625cbf9a0cc1084
Evgeniy Dimitriev <dimitriev_ev@mail.ru>
2025-07-02 21:04:51 +0500

    Refactor test_logic tests

--- notes/tests/test_logic.py
+++ notes/tests/test_logic.py
@@ -1,6 +1,7 @@
 from http import HTTPStatus
 
 from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 from django.urls import reverse
 
@@ -22,6 +23,11 @@ class TestNoteLogic(TestCase):
             author=cls.user_1,
             slug='test-note'
         )
+        cls.note_auto_slug = Note.objects.create(
+            title='Автоматический slug',
+            text='Текст',
+            author=cls.user_2
+        )
 
     def test_user_can_create_note(self):
         """Авторизованный пользователь может создать заметку."""
@@ -35,6 +41,34 @@ class TestNoteLogic(TestCase):
         response = self.client.post(url, data=form_data)
         self.assertRedirects(response, reverse('notes:success'))
         self.assertTrue(Note.objects.filter(slug='new-note').exists())
+
+    def test_anonymous_user_cannot_create_note(self):
+        """Анонимный пользователь не может создать заметку."""
+        url = reverse('notes:add')
+        form_data = {
+            'title': 'Анонимная заметка',
+            'text': 'Текст',
+            'slug': 'anon-note'
+        }
+        response = self.client.post(url, data=form_data)
+        login_url = reverse('users:login')
+        self.assertRedirects(response, f'{login_url}?next={url}')
+        self.assertFalse(Note.objects.filter(slug='anon-note').exists())
+
+    def test_slug_auto_creation(self):
+        """Проверка автоматического создания slug."""
+        self.assertEqual(self.note_auto_slug.slug, 'avtomaticheskij-slug')
+
+    def test_unique_slug_validation(self):
+        """Проверка валидации уникальности slug."""
+        with self.assertRaises(ValidationError):
+            note = Note(
+                title='Новая заметка',
+                text='Текст',
+                author=self.user_1,
+                slug='test-note'
+            )
+            note.full_clean()
 
     def test_author_can_edit_note(self):
         """Автор может получить страницу редактирования заметки."""
@@ -51,25 +85,12 @@ class TestNoteLogic(TestCase):
         self.assertRedirects(response, reverse('notes:success'))
         self.assertFalse(Note.objects.filter(pk=self.note.pk).exists())
 
-    def test_anonymous_user_cannot_create_note(self):
-        """Анонимный пользователь не может создать заметку."""
-        url = reverse('notes:add')
-        form_data = {
-            'title': 'Анонимная заметка',
-            'text': 'Текст',
-            'slug': 'anon-note'
-        }
-        response = self.client.post(url, data=form_data)
-        login_url = reverse('users:login')
-        self.assertRedirects(response, f'{login_url}?next={url}')
-        self.assertFalse(Note.objects.filter(slug='anon-note').exists())
-
     def test_other_user_cannot_edit_someone_elses_note(self):
         """Авторизованный пользователь не может редактировать чужую заметку."""
         self.client.force_login(self.user_2)
         url = reverse('notes:edit', args=(self.note.slug,))
         response = self.client.post(url, data={
-            'title': 'Попытка взлома',
+            'title': 'Попытка',
             'text': 'Новый текст',
             'slug': 'test-note'
         })
